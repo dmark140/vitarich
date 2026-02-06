d671aa79b3a1b11f40f691970be44e12910f162c
dmark140 <famolerasd@gmail.com>
2026-02-06 11:22:18 +0800

    pushx

--- app/home/StartUp.tsx
+++ app/home/StartUp.tsx
@@ -8,7 +8,6 @@ export default function StartUp() {
         { label: 'Dashboard', icon: <Sparkles size={18} className="text-orange-500 dark:text-orange-400" /> },
         { label: 'Hatchiry', icon: <EggFried size={18} /> },
     ];
-
     return (
         <div className="flex flex-col items-start justify-center   text-slate-900 dark:text-white p-8 mr-4  transition-colors duration-300">
             <div className="max-w-2xl w-full space-y-8">
@@ -16,7 +15,7 @@ export default function StartUp() {
                 {/* Header Section */}
                 <header className="space-y-2">
                     <p className="text-xl font-medium ">
-                        Hi Deanmark
+                        Hi There!
                     </p>
                     <h1 className="text-4xl  font-semibold tracking-tight">
                         Where should we start?
@@ -27,11 +26,9 @@ export default function StartUp() {
                 <div className="flex flex-col items-start space-y-3">
                     {buttons.map((btn, index) => (
                         <Button
+                            onClick={() => console.log("test")}
                             key={index}
-                            className={`
-                flex items-center gap-3 px-6 py-3  h-10 rounded-full transition-all duration-200
-             
-              `}
+                            className={`flex items-center gap-3 px-6 py-3  h-10 rounded-full transition-all duration-200`}
                         >
                             <span>{btn.icon}</span>
                             <span className="text-lg font-medium group-hover:dark:text-white">
