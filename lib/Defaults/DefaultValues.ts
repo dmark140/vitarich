2656091788bcb62730a38d8349c29fc2216a2a7c
kurt091913 <31057806+kurt091913@users.noreply.github.com>
2026-02-10 13:58:57 +0800

    02-10-2026
    UI of doc dispatch

--- lib/Defaults/DefaultValues.ts
+++ lib/Defaults/DefaultValues.ts
@@ -53,6 +53,7 @@ export const NavFolders = [
           { type: "Module", title: "Egg Transfer Process", url: "/a_baja/eggtransfer" },
           { type: "Module", title: "Egg Hatchery Process", url: "/a_baja/egghatcheryprocessform" },
           { type: "Module", title: "Chick Grading", url: "/a_baja/chickgrading" },
+          { type: "Module", title: "Doc Dispatch", url: "/a_baja/docdispatch" },
         ],
       },
       {
